99d2a717c6f489567960575e0e5078f17758f043
Max Nachlinger <max.nachlinger@gmail.com>
2022-08-01 21:53:18 -0700

    Added in tonic and a simple echo gRPC route

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,53 @@
-mod settings;
 mod routes;
+mod settings;
 
+use crate::routes::{get_echo, openapi, post_echo};
 use crate::settings::{get_settings, AppOptions};
 use actix_web::{middleware, App, HttpServer};
-use crate::routes::{get_echo, openapi, post_echo};
+use echo::echo_server::{Echo, EchoServer};
+use echo::MessageToEcho;
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod echo {
+    tonic::include_proto!("echo");
+}
+
+#[derive(Debug, Default)]
+pub struct EchoService {}
+
+#[tonic::async_trait]
+impl Echo for EchoService {
+    async fn echo_message(
+        &self,
+        request: Request<MessageToEcho>,
+    ) -> Result<Response<MessageToEcho>, Status> {
+        Ok(Response::new(MessageToEcho {
+            message: request.into_inner().message,
+        }))
+    }
+}
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() {
     let settings = get_settings();
-    let AppOptions { host, port } = settings.app;
+    let AppOptions {
+        host,
+        port,
+        socket_address,
+    } = settings.app;
+
+    let parsed_socket_address = socket_address.parse().expect("Cannot parse socket address");
+    let echo_service = EchoService::default();
 
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    log::info!("Listening at: {}:{}", &host, &port);
+    tokio::spawn(async move {
+        Server::builder()
+            .add_service(EchoServer::new(echo_service))
+            .serve(parsed_socket_address)
+            .await
+            .expect("Cannot start gRPC server")
+    });
 
     HttpServer::new(|| {
         App::new()
@@ -21,7 +56,9 @@ async fn main() -> std::io::Result<()> {
             .service(post_echo)
             .service(openapi)
     })
-    .bind((host, port))?
-    .run()
-    .await
+        .bind((host, port))
+        .expect("Cannot start REST server")
+        .run()
+        .await
+        .expect("Cannot start REST server")
 }
--- src/settings.rs
+++ src/settings.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 pub struct AppOptions {
     pub host: String,
     pub port: u16,
+    pub socket_address: String,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -13,6 +14,7 @@ pub struct Settings {
 
 const DEFAULT_HOST: &str = "127.0.0.1";
 const DEFAULT_PORT: u16 = 8080;
+const DEFAULT_SOCKET_ADDRESS: &str = "[::1]:10000";
 
 pub fn get_settings() -> Settings {
     let host = option_env!("HOST").unwrap_or(DEFAULT_HOST).into();
@@ -20,8 +22,15 @@ pub fn get_settings() -> Settings {
         .unwrap_or(DEFAULT_PORT.to_string().as_ref())
         .parse::<u16>()
         .unwrap_or(DEFAULT_PORT);
+    let socket_address = option_env!("SOCKET_ADDRESS")
+        .unwrap_or(DEFAULT_SOCKET_ADDRESS)
+        .into();
 
     Settings {
-        app: AppOptions { port, host },
+        app: AppOptions {
+            port,
+            host,
+            socket_address,
+        },
     }
 }
